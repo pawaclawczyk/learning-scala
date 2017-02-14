257ecd58a5b76f64d016f0ffcc7b6789537a6b05
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2017-02-14 15:39:06 +0100

    Functional programming in Scala. Week 1. Example Assignement Solution.

--- functional_programming_in_scala/week1/example/src/main/scala/example/Lists.scala
+++ functional_programming_in_scala/week1/example/src/main/scala/example/Lists.scala
@@ -23,8 +23,12 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
-  
+    def sum(xs: List[Int]): Int = {
+      def sumWithAcc(xs: List[Int], acc: Int) : Int = if (xs.isEmpty) acc else sumWithAcc(xs.tail, acc + xs.head)
+
+      sumWithAcc(xs, 0)
+    }
+
   /**
    * This method returns the largest element in a list of integers. If the
    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
@@ -38,5 +42,9 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      def maxWithAcc(xs: List[Int], acc: Int): Int = if (xs.isEmpty) acc else maxWithAcc(xs.tail, if (acc > xs.head) acc else xs.head)
+
+      if (xs.isEmpty) throw new java.util.NoSuchElementException else maxWithAcc(xs.tail, xs.head)
+    }
   }
--- functional_programming_in_scala/week1/example/src/test/scala/example/ListsSuite.scala
+++ functional_programming_in_scala/week1/example/src/test/scala/example/ListsSuite.scala
@@ -47,7 +47,7 @@ import org.scalatest.junit.JUnitRunner
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +72,7 @@ import org.scalatest.junit.JUnitRunner
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2) // Fix me, please!
   }
 
   /**
@@ -117,10 +117,25 @@ import org.scalatest.junit.JUnitRunner
     assert(sum(List(1,2,0)) === 3)
   }
 
+  test("sum of empty list is zero") {
+    assert(sum(List()) === 0)
+  }
+
+  test("sum of negative numbers") {
+    assert(sum(List(-3, -5, -1)) === -9)
+  }
+
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
   }
 
+  test("max of empty list throws exception") {
+    intercept[java.util.NoSuchElementException]{
+      max(List())
+    }
+  }
 
-
+  test("max of negative numbers") {
+    assert(max(List(-5, -9, -1)) === -1)
+  }
 }
